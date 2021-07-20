bf7e5d9fc6cebc8f91571bd9da3089473511c13c
harinkj <harinkj@gmail.com>
2021-07-20 09:09:34 -0400

    Create run.cmd
    cmd file for compiling and running Test.java with paramters
    3 NONE - means 3 minutes delay timer and non stopping

---
+++ robot/run.cmd
@@ -0,0 +1,4 @@
+set JAVA_HOME= C:\project\software\jdk-14.0.1
+set PATH=.;%JAVA_HOME%\bin;%PATH%
+C:\project\software\jdk-14.0.1\bin\javac Test.java
+C:\project\software\jdk-14.0.1\bin\java Test 3 NONE
